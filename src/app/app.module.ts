2a88c52db6878109f49cb324c15ab4283e114ba9
kenethPs <68650722+kenethPs@users.noreply.github.com>
2023-07-17 22:32:48 -0500

    componentes para el dashboard y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CardsComponent } from './cards/cards.component';
+import { BarColorsComponent } from './bar-colors/bar-colors.component';
+import { InformationComponent } from './information/information.component';
+import { BarnavComponent } from './barnav/barnav.component';
+import { LoginComponent } from './login/login.component';
+import { GraphicComponent } from './graphic/graphic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    CardsComponent,
+    BarColorsComponent,
+    InformationComponent,
+    BarnavComponent,
+    LoginComponent,
+    GraphicComponent
   ],
   imports: [
     BrowserModule,
