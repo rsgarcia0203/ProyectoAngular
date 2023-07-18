7d7f3c1927895ebc4ba5775abb5eedaf4841742a
Ronny Garcia <rsgarcia@espol.edu.ec>
2023-07-18 11:10:14 -0500

    Refactor code
    Se refactorizó el código y se cambió la estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import axios from 'axios';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,12 +9,11 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { CardsComponent } from './cards/cards.component';
 import { BarColorsComponent } from './bar-colors/bar-colors.component';
-import { InformationComponent } from './information/information.component';
+import { InformationComponent } from './components/information/information.component';
 import { BarnavComponent } from './barnav/barnav.component';
-import { GraphicComponent } from './graphic/graphic.component';
+import { GraphicComponent } from './components/graphic/graphic.component';
 
 import { LoginComponent } from './components/login/login.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LogoutComponent } from './components/logout/logout.component';
 
 @NgModule({
