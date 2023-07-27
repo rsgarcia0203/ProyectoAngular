1aef10738cec7759e01dde94e6d9622ef52e5d37
Ronny Garcia <rsgarcia@espol.edu.ec>
2023-07-27 12:07:47 -0500

    Agregar Servicios
    Se agregaron e implementaron los servicios para proveer la data a los demás componentes y proveer la información del usuario.

---
+++ src/app/providers/DataService/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+// Importación de Axios
+import { Axios } from 'axios';
+import { DataContext } from 'src/app/interfaces/idata-context';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService {
+
+  private URL:string = 'https://angulardawm-default-rtdb.firebaseio.com/data_context.json';
+
+  constructor(private axios:Axios) { }
+
+  //Método con la petición HTTP
+  public async getData():Promise<DataContext[]> {
+    return this.axios.get(this.URL)
+                      .then((response) => response.data as DataContext[]) ;
+  }
+}
