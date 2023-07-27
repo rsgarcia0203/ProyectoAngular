d096ed73a3b87c8f45f2bf1a21dcc774bb13cc10
Ronny Garcia <rsgarcia@espol.edu.ec>
2023-07-27 15:14:39 -0500

    Añadir Enrutamiento
    Se añadió enrutamiento para la aplicación, creando las rutas de home, login y pagenotfound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
+import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InformationComponent } from './components/information/information.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  
-
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
