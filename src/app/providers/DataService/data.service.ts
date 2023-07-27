77fc21365eb69f512aee1bd49f197abc6722354e
Ronny Garcia <rsgarcia@espol.edu.ec>
2023-07-27 15:16:55 -0500

    Añadir Servicio para Datos
    Se añadió un servicio que permitirá la extracción de los datos desde firebase

--- src/app/providers/DataService/data.service.ts
+++ src/app/providers/DataService/data.service.ts
@@ -5,18 +5,22 @@ import { Axios } from 'axios';
 import { DataContext } from 'src/app/interfaces/idata-context';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class DataService {
+  private URL: string =
+    'https://angulardawm-default-rtdb.firebaseio.com/data_context.json';
 
-  private URL:string = 'https://angulardawm-default-rtdb.firebaseio.com/data_context.json';
-
-  constructor(private axios:Axios) { }
+  constructor(private axios: Axios) {}
 
   //Método con la petición HTTP
-  public async getData():Promise<DataContext[]> {
-    return this.axios.get(this.URL)
-                      .then((response) => response.data as DataContext[]) ;
+  public async getData(): Promise<DataContext[]> {
+    return this.axios
+      .get(this.URL)
+      .then((response) => response.data as DataContext[])
+      .catch((error) => {
+        console.log(error);
+        return [];
+      });
   }
 }
