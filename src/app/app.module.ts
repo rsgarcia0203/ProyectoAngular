271e4e2ab286ceeac2027a94348d86bd8ed679c5
Ronny Garcia <rsgarcia@espol.edu.ec>
2023-07-28 15:38:15 -0500

    Controlar Estado de usuario
    Se controlará el estado del usuario desde el componente del navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,7 @@ import { LogoutComponent } from './components/logout/logout.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { ChartComponent } from './components/charts/chart/chart.component';
+import { ChartComponent } from './components/chart/chart.component';
 
 @NgModule({
   declarations: [
--- src/app/components/barnav/barnav.component.ts
+++ src/app/components/barnav/barnav.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '@angular/fire/auth';
+import { AuthService } from 'src/app/providers/AuthService/auth.service';
 
 @Component({
   selector: 'app-barnav',
   templateUrl: './barnav.component.html',
   styleUrls: ['./barnav.component.css']
 })
-export class BarnavComponent {
+export class BarnavComponent implements OnInit {
+  public user:User | null = null;
+  
+  constructor (private readonly auth:AuthService){ }
 
+  logout() {
+    this.auth.logout();
+  }
+
+  ngOnInit(): void {
+    this.auth.user.subscribe(user => {
+      this.user = user;
+    });
+  }
 }
